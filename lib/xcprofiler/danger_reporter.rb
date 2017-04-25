cdcd37f872d058c1c5e9b33d9f44d7e14b85eb56
giginet <giginet.net@gmail.com>
2017-04-25 19:15:46 +0900

    Implement DangerXcprofiler

---
+++ lib/xcprofiler/danger_reporter.rb
@@ -0,0 +1,37 @@
+require 'xcprofiler'
+require 'pathname'
+
+module Danger
+  class DangerReporter < Xcprofiler::AbstractReporter
+    def initialize(dangerfile, thresholds, inline_mode, working_dir)
+      super({})
+      @dangerfile = dangerfile
+      @thresholds = thresholds
+      @inline_mode = inline_mode
+      @working_dir = working_dir
+    end
+
+    def report!(executions)
+      executions.each do |execution|
+        options = {}
+        if @inline_mode
+          options[:file] = relative_path(execution.path)
+          options[:line] = execution.line
+        end
+        message = "`#{execution.method_name}` takes #{execution.time} ms to build"
+        if execution.time >= @thresholds[:fail]
+          @dangerfile.fail(message, options)
+        elsif execution.time >= @thresholds[:warn]
+          @dangerfile.warn(message, options)
+        end
+      end
+    end
+
+    private
+
+    def relative_path(path)
+      working_dir = Pathname.new(@working_dir)
+      Pathname.new(path).relative_path_from(working_dir).to_s
+    end
+  end
+end
--- lib/xcprofiler/plugin.rb
+++ lib/xcprofiler/plugin.rb
@@ -1,16 +1,53 @@
+require 'xcprofiler'
+require_relative 'danger_reporter'
+
 module Danger
   class DangerXcprofiler < Plugin
 
-    # An attribute that you can read/write from your Dangerfile
+    # Defines path for working directory
+    # Default value is `Dir.pwd`
+    # @params    [String] value
+    # @return    [String]
     #
-    # @return   [Array<String>]
-    attr_accessor :my_attribute
+    attr_accessor :working_dir
 
-    # A method that you can call from your Dangerfile
-    # @return   [Array<String>]
+    # Defines threshold of completion time (ms) to assert warning/failure
+    # Default value is `{ warn: 100, fail: 500 }`
+    # @param    [Hash<String, String>] value
+    # @return   [Hash<String, String>]
     #
-    def warn_on_mondays
-      warn 'Trying to merge code on a Monday' if Date.today.wday == 1
+    attr_accessor :thresholds
+
+    # Defines if using inline comment to assert
+    # Default value is `true`
+    # @param    [Boolean] value
+    # @return   [Boolean]
+    #
+    attr_accessor :inline_mode
+
+    def report(product_name)
+      profiler = Xcprofiler::Profiler.by_product_name(product_name)
+      profiler.reporters = [
+          DangerReporter.new(@dangerfile, thresholds, inline_mode, working_dir)
+      ]
+      profiler.report!
+    rescue Xcprofiler::DerivedDataNotFound, Xcprofiler::BuildFlagIsNotEnabled => e
+      warn(e.message)
+    end
+
+    private
+
+    def working_dir
+      @working_dir || Dir.pwd
+    end
+
+    def thresholds
+      @thresholds || { warn: 100, fail: 500 }
+    end
+
+    def inline_mode
+      return !!@inline_mode if @inline_mode
+      true
     end
   end
 end
